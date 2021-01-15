a569f1d23a62aa195c267662cc8552863e5d583a
omkars7 <54521647+omkars7@users.noreply.github.com>
2021-01-15 19:13:52 +0530

    Add files via upload

---
+++ 123/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudMVCADO.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        public int deptId { get; set; }
+        public string deptName { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ 123/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.
+
+namespace CrudMVCADO.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        //SqlConnection con = new SqlConnection("Data Source=DESKTOP-2NQS68P;Initial Catalog=EmployeeDB;Integrated Security=True");
+
+        
+
+    }
+}
